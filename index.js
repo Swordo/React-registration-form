18c75c68253fdad8ebe82c4f88172ae22cb0587c
Swordo <swordo95@icloud.com>
2019-01-08 23:58:38 +0530

    done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const app = express();
+const port = process.env.PORT || 1234;
+
+
+app.get('/', (req, res) => {
+    res.send('workin')
+})
+app.listen(port, (err) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log('Server is running...')
+    }
+})
